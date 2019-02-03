12cbf7fd3195e6ad27cfce5a872e864ef5dbfb47
styczynski <piotrsty1@gmail.com>
2019-02-03 19:35:41 +0100

    Switch generation to -g2005

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "$opwd"
 for testfile in `find . -name "Test*"`;
 do
   cd "$(dirname $testfile)"
-  iverilog -g2012 "$(basename $testfile)"
+  iverilog -g2005 "$(basename $testfile)"
   if [ "$?" != "0" ]; then
     cd "$opwd"
     exit 1
